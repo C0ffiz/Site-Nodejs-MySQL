6d6197a838c8af446e31214c0e73a8f43edf91b1
C0ffiz <93149828+C0ffiz@users.noreply.github.com>
2022-05-27 23:11:48 -0300

    .

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+
+const app = express()
+
+const conn = require('./db/conn')
+
+const Aluno = require('./models/Aluno')
+
+const alunoRoutes = require('./routes/alunosRoutes')
+
+app.engine('handlebars', exphbs())
+app.set('view engine', 'handlebars')
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  }),
+)
+
+app.use(express.json())
+
+app.use(express.static('public'))
+
+app.use('/alunos', alunoRoutes)
+
+conn
+  .sync()
+  .then(() => {
+    app.listen(3000)
+  })
+  .catch((err) => console.log(err))
---
+++ routes/alunosRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const router = express.Router()
+const AlunoController = require('../controllers/AlunoController')
+
+router.get('/', AlunoController.showAlunos)
+router.get('/add', AlunoController.createAluno)
+router.post('/add', AlunoController.createAlunoSave)
+router.post('/remove', AlunoController.removeAluno)
+router.get('/edit/:id', AlunoController.updateAluno)
+router.post('/edit', AlunoController.updateAlunoPost)
+
+router.post('/updatestatus', AlunoController.changeStatus)
+
+module.exports = router
