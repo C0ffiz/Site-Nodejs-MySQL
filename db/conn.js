a1f0d1424cd23e2d0d03b4a1421027fd77e7d06c
C0ffiz <93149828+C0ffiz@users.noreply.github.com>
2022-06-03 00:42:34 -0300

    changes on homepage

--- db/conn.js
+++ db/conn.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize')
 
-const sequelize = new Sequelize('node_mvc', 'root', '1234', {
+const sequelize = new Sequelize('node_mvc', 'root', 'tartaruga', {
     host: 'localhost',
     dialect: 'mysql',
 })
